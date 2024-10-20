bda442e5109010f22bd73202476a263e6b5dc68a
namebighourse <masterskihila@gmail.com>
2024-10-20 16:28:02 +0300

    feat: adding code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const one = ["zero","one","two","three","four","five","six","seven","eight","nine",];
+    const teens = ["ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen",];
+    const tens = ["","","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety",];
+    const hundreds = "hundred";
+    const tenB = Math.floor(number / 10);
+    const NumberC = (number % 10 );
+    const hundredR = (number % 100);
+    const hundred = Math.floor(number / 100);
+if (number < 10) {
+     return(one[number])
+
+}else if(number < 20) {
+    return(teens[number - 10])
+}else if (number <100){
+    return tens[tenB] + (NumberC ? " " + one[NumberC] : "")
+
+}  else if (number < 1000){
+return one[hundred] + " " + 'hundred' + (hundredR ? " " + toReadable(hundredR) : "" ) 
+};
+
 }
+ 
